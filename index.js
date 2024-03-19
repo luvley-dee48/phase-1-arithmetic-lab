5fcdca3f81407c1d4d40866b86602f5f7f52e07a
deborah <fabbydebby@gmail.com>
2024-03-19 22:14:37 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply); 
 // Write your code here
+let random = Math.ceil(Math.random() * 100);
+console.log(random);
+
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+if (mod === 4) {
+    console.log('The reminder is 4.');
+}else {
+    console.log('The reminder is not 4.');
+}
+console.log("Remainder:", mod);
+
+let numbers = [2,6,8,10,12,14,16,18,20];
+let max = Math.max(2,4,6,8,10,12,14,16,18,20);
+console.log("The highest number in the set is:", max);
